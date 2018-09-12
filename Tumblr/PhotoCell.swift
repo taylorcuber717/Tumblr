0eb70664db27a91454f29431b387253935f0c0ab
Taylor McLaughlin <taylormclaughlin@dhcp-10-134-218-32.uawifi.arizona.edu>
2018-09-12 14:06:08 -0700

    Photo feed working

---
+++ Tumblr/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  Tumblr
+//
+//  Created by Taylor McLaughlin on 9/12/18.
+//  Copyright © 2018 Taylor McLaughlin. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
